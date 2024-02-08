aa85d393129af8eef65cc43a2aae9dbe74f34e18
Fasih Muhammad <82199241+fasihmuhammadvirk@users.noreply.github.com>
2024-02-09 01:06:06 +0500

    working with models and database

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render  
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 # Create your views here.
 
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
+
 
 def home(request):
-    context = {'rooms':rooms}
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
