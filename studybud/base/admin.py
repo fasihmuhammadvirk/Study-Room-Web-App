ad203d2d563f06ae653d13875fb24ececc3325c6
Fasih Muhammad <82199241+fasihmuhammadvirk@users.noreply.github.com>
2024-03-02 20:43:51 +0500

    Update
    Working on the CURD operations and the search bar.

--- studybud/base/admin.py
+++ studybud/base/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room
+from .models import Room, Topic, Messgae
 
-admin.site.register(Room)
+admin.site.register(Room)  
+admin.site.register(Topic)
+admin.site.register(Messgae)
