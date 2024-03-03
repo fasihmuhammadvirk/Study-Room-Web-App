c5fb91c7cffa886e786c75b979b94405afcbaddc
Fasih Muhammad <82199241+fasihmuhammadvirk@users.noreply.github.com>
2024-03-03 23:19:05 +0500

    user login

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
@@ -10,6 +12,20 @@ from .forms import RoomForm
 #     {'id': 2, 'name': 'Design with me'},
 #     {'id': 3, 'name': 'Frontend developers'},
 # ]
+
+def loginPage(request):
+    if request.method == 'POST':
+        # email = reqest.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+
+        except:
+            messages.error(request, 'User does not exist')
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
